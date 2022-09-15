631acd97231138c95c5e08cd47c245406944e640
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-09-14 22:07:59 -0500

    trying to figure out API call for elevation data

--- dataToTerrain3d.js
+++ dataToTerrain3d.js
@@ -22,21 +22,33 @@ allows for saving maps with save button (glsl model)
     let elevMatrix = []; //{x: 0, y: 0, lat: coord.lat, long: coord.long, elev: 0}
 
     function buildElevMatrix(posit) { // posit is an object {latitude to 2 decimal places, longitude to 2 decimal places}
-        for (let i = 0; i < 10; i ++) {
-            for (let j = 0; j < 10; j ++) {
+        for (let i = 0; i < 2; i ++) {
+            for (let j = 0; j < 2; j ++) {
                 let latitude = (posit.lat - (i/100.0)).toFixed(3);
                 let longitude = (posit.long + (j/100.0)).toFixed(3);
-                let elev = fetch("https://api.open-elevation.com/api/v1/lookup?locations="+latitude+","+longitude, {method: 'POST'})
-                    .then(Result => Result.json())
-                    .then =(string => {
-                    console.log(string);
-                    console.log(`Title of our response :  ${string.title}`);
-                    })
-                    /*.catch(errorMsg => { console.log(errorMsg);})*/;
-                console.log(latitude, longitude, elev);
+                let url = "https://api.open-elevation.com/api/v1/lookup";
+                let bod = '{"locations": [{"latitude": ' + latitude + ', "longitude": ' + longitude + '}]}';
+                let elev = getElevationFromAPI(url, bod);
+                console.log(elev);
 //                elevMatrix.push({x: i, y: j, lat: latitude, long: longitude});
             }
         }
+    }
+
+    async function getElevationFromAPI(url, data) {
+        let response = fetch(url, {
+            method: 'POST',
+            mode: 'no-cors',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then((response) => response.json())
+            .then ((data) => console.log(data));
+        /*.catch(errorMsg => { console.log(errorMsg);})*/;
+        return response;
     }
 
     buildElevMatrix(coord);
