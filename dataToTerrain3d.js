6c580b4d7658bd2cbc86201831cfeef2beb5cd1f
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-09-15 13:11:36 -0500

    Finally got it to work.  Need to researh fetch init block.

--- dataToTerrain3d.js
+++ dataToTerrain3d.js
@@ -19,41 +19,56 @@ allows for saving maps with save button (glsl model)
 
 (function() {
     let coord = {lat: 37.500, long: 127.600}; // latitude and longitude to 2 decimal places
-    let elevMatrix = []; //{x: 0, y: 0, lat: coord.lat, long: coord.long, elev: 0}
+//    let elevMatrix = []; //{x: 0, y: 0, lat: coord.lat, long: coord.long, elev: 0}
+    let url = "";
+    let bod = "";
 
-    function buildElevMatrix(posit) { // posit is an object {latitude to 2 decimal places, longitude to 2 decimal places}
+    function buildRequest(posit) { // posit is an object {latitude to 2 decimal places, longitude to 2 decimal places}
+        url = "https://api.open-elevation.com/api/v1/lookup";
+        let bodStr = '{"locations": [';
         for (let i = 0; i < 2; i ++) {
             for (let j = 0; j < 2; j ++) {
                 let latitude = (posit.lat - (i/100.0)).toFixed(3);
                 let longitude = (posit.long + (j/100.0)).toFixed(3);
-                let url = "https://api.open-elevation.com/api/v1/lookup";
-                let bod = '{"locations": [{"latitude": ' + latitude + ', "longitude": ' + longitude + '}]}';
-                let elev = getElevationFromAPI(url, bod);
-                console.log(elev);
+                bodStr += '{"latitude": ' + latitude + ', "longitude": ' + longitude + '},'
 //                elevMatrix.push({x: i, y: j, lat: latitude, long: longitude});
             }
         }
+        bodStr = bodStr.slice(0,bodStr.length-1);
+        bodStr += ']}';
+        console.log(url);
+        console.log(bodStr);
+        bod = JSON.parse(bodStr);
+        console.log(bod);
     }
-
     //https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
     //https://github.com/Jorl17/open-elevation/blob/master/docs/api.md
     //https://api.open-elevation.com/api/v1/lookup?locations=37.500,127.600
 
+
+    // let elev = getElevationFromAPI(url, bod);
+    // console.log(elev);
+
+
+
     async function getElevationFromAPI(url, data) {
-        let response = fetch(url, {
+        let resp = fetch(url, {
             method: 'POST',
-            mode: 'no-cors',
+ //           mode: 'same-origin',
             credentials: 'same-origin',
             headers: {
+                'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
         })
             .then((response) => response.json())
-            .then ((data) => console.log(data));
-        /*.catch(errorMsg => { console.log(errorMsg);})*/;
-        return response;
+            //.then ((data) => console.log(data))
+            .catch(errorMsg => { console.log(errorMsg);});
+        return resp;
     }
 
-    buildElevMatrix(coord);
+    buildRequest(coord);
+    console.log(getElevationFromAPI(url, bod));
+
 } ());
