1b72d068cfd8fcde0a6ac0a9aa88c515059b7583
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-09-15 21:52:46 -0500

    changed to terrain generation from cube

--- dataToTerrain3d.js
+++ dataToTerrain3d.js
@@ -50,18 +50,27 @@ parse API result JSON, building elevation grid
     let elevMatrix = [];
     let url = "";
     let bod = "";
+    let pointsDim = 50; // make a power of 10 please unless you wanna refactor
+    let totalPoints = pointsDim * pointsDim;
+    let gap = 0.05 / pointsDim;
+     let pallette = {
+         p1: { x: 0.61, y: 0.87, z: 0.33 },
+         p2: { x: 0.72, y: 0.51, z: 0.3 },
+         p3: { x: 0.38, y: 0.4, z: 0.74 },
+         p4: { x: 0.38, y: 0.36, z: 0.27 },
+     };
 
     function buildRequest(posit) { // posit is an object {latitude to 2 decimal places, longitude to 2 decimal places}
         url = "https://api.open-elevation.com/api/v1/lookup";
         let bodStr = '{"locations": [';
-        for (let i = 0; i < 10; i ++) {
-            for (let j = 0; j < 10; j ++) {
-                let latitude = (posit.lat - (i/100.0)).toFixed(3);
-                let longitude = (posit.long + (j/100.0)).toFixed(3);
+        for (let i = 0; i < pointsDim; i ++) {
+            for (let j = 0; j < pointsDim; j ++) {
+                let latitude = (posit.lat - (i*gap)).toFixed(3);
+                let longitude = (posit.long + (j*gap)).toFixed(3);
                 bodStr += '{"latitude": ' + latitude + ', "longitude": ' + longitude + '},'
             }
         }
-        bodStr = bodStr.slice(0,bodStr.length-1);
+        bodStr = bodStr.slice(0, bodStr.length-1);
         bodStr += ']}';
         console.log(url);
         console.log(bodStr);
@@ -86,7 +95,7 @@ parse API result JSON, building elevation grid
             .then ((data) => {
  //               console.log(data.results[0].elevation);  // elev for one point
                 console.log("From inside: ");
-                for(let i = 0; i < 100; i++) {elevArray.push(data.results[i].elevation)};
+                for(let i = 0; i < totalPoints; i++) {elevArray.push(data.results[i].elevation)};
                 console.log(elevArray);
                 init();
              })
@@ -103,7 +112,8 @@ parse API result JSON, building elevation grid
         buildRenderer();
         container = renderer.domElement;
         document.body.appendChild(container);
-        buildTerrain();
+        // buildTerrain();
+        buildTerrainFromData();
         addOrbitControls();
         window.addEventListener("resize", onWindowResize);
         animate();
@@ -178,11 +188,113 @@ parse API result JSON, building elevation grid
         console.log(scene);
     }
 
+    function buildTerrainFromData() {
+        let maxElev = Math.max(...elevMatrix);
+        //resize elev data
+        for (let i = 0; i < elevMatrix.length; i ++) {
+            elevMatrix[i] /= 50.0;
+        }
+        console.log('After shift: ' + elevMatrix);
+        // build
+        let vertices = [];
+        for (let i = 0; i < pointsDim - 1; i ++) {
+            for (let j = 0; j < pointsDim - 1; j++) {
+                let idx = (i * pointsDim) + j;
+                let cv = getPalletteSample(elevMatrix[idx]/maxElev, pallette);
+                let colVal = [cv.r, cv.g, cv.b];
+                vertices.push({
+                    pos: [i - pointsDim / 2, elevMatrix[i * pointsDim + j], j - pointsDim / 2],
+                    col: colVal,
+                });
+                vertices.push({
+                    pos: [
+                        i + 1 - pointsDim / 2,
+                        elevMatrix[(i + 1) * pointsDim + j],
+                        j - pointsDim / 2,
+                    ],
+                    col: colVal,
+                });
+                vertices.push({
+                    pos: [
+                        i - pointsDim / 2,
+                        elevMatrix[i * pointsDim + (j + 1)],
+                        j + 1 - pointsDim / 2,
+                    ],
+                    col: colVal,
+                });
+                vertices.push({
+                    pos: [
+                        i + 1 - pointsDim / 2,
+                        elevMatrix[(i + 1) * pointsDim + j],
+                        j - pointsDim / 2,
+                    ],
+                    col: colVal,
+                });
+                vertices.push({
+                    pos: [
+                        i + 1 - pointsDim / 2,
+                        elevMatrix[(i + 1) * pointsDim + (j + 1)],
+                        j + 1 - pointsDim / 2,
+                    ],
+                    col: colVal,
+                });
+                vertices.push({
+                    pos: [
+                        i - pointsDim / 2,
+                        elevMatrix[i * pointsDim + (j + 1)],
+                        j + 1 - pointsDim / 2,
+                    ],
+                    col: colVal,
+                });
+            }
+        }
+        const positions = [];
+        const colors = [];
+        for (const vertex of vertices) {
+            positions.push(...vertex.pos);
+            colors.push(...vertex.col);
+        }
+        let geometry = new THREE.BufferGeometry();
+        geometry.setAttribute(
+            "position",
+            new THREE.BufferAttribute(new Float32Array(positions), 3)
+        );
+        geometry.setAttribute(
+            "color",
+            new THREE.BufferAttribute(new Float32Array(colors), 3)
+        );
+
+        geometry.computeVertexNormals();
+
+        material = new THREE.MeshPhongMaterial({
+            vertexColors: THREE.VertexColors,
+            side: THREE.DoubleSide,
+        });
+
+        let mesh = new THREE.Mesh(geometry, material);
+        scene.add(mesh);
+    }
+
     function addOrbitControls() {
         controls = new THREE.OrbitControls(camera, container);
         controls.minDistance = 5;
-        controls.maxDistance = 50;
+        controls.maxDistance = 1500;
         controls.autoRotate = true;
+    }
+
+    function getPalletteSample(t, pallette) {
+        // adapted from Inigio Quilez: https://iquilezles.org/articles/palettes/
+        let re =
+            pallette.p1.x +
+            pallette.p2.x * Math.cos(2 * Math.PI * (pallette.p3.x * t + pallette.p4.x));
+        let gr =
+            pallette.p1.y +
+            pallette.p2.y * Math.cos(2 * Math.PI * (pallette.p3.y * t + pallette.p4.y));
+        let bl =
+            pallette.p1.z +
+            pallette.p2.z * Math.cos(2 * Math.PI * (pallette.p3.z * t + pallette.p4.z));
+        let c = new THREE.Color(re, gr, bl);
+        return c;
     }
 
     function onWindowResize() {
---
+++ terrain.js
@@ -0,0 +1,117 @@
+function buildTerrain() {   // from Baconbits Terranium
+
+    //  put all of your geometry and materials in here
+    maxElev = 0;
+    //  calculateNoiseGrid();
+    let terrainData = calculateNoiseGridV2(0);
+    lowerData = terrainData.grid;
+    ldMax = terrainData.max;
+
+    let vertices = [];
+
+    for (let i = 0; i < width - 1; i++) {
+        for (let j = 0; j < height - 1; j++) {
+            if (maxElev < lowerData[i * height + j]) {
+                maxElev = lowerData[i * height + j];
+            }
+
+            let cv = getPalletteSample(lowerData[i * height + j] / ldMax, pallette);
+            let colVal = [cv.r, cv.g, cv.b];
+
+            vertices.push({
+                pos: [i - height / 2, lowerData[i * height + j], j - width / 2],
+                col: colVal,
+            });
+            vertices.push({
+                pos: [
+                    i + 1 - height / 2,
+                    lowerData[(i + 1) * height + j],
+                    j - width / 2,
+                ],
+                col: colVal,
+            });
+            vertices.push({
+                pos: [
+                    i - height / 2,
+                    lowerData[i * height + (j + 1)],
+                    j + 1 - width / 2,
+                ],
+                col: colVal,
+            });
+            vertices.push({
+                pos: [
+                    i + 1 - height / 2,
+                    lowerData[(i + 1) * height + j],
+                    j - width / 2,
+                ],
+                col: colVal,
+            });
+            vertices.push({
+                pos: [
+                    i + 1 - height / 2,
+                    lowerData[(i + 1) * height + (j + 1)],
+                    j + 1 - width / 2,
+                ],
+                col: colVal,
+            });
+            vertices.push({
+                pos: [
+                    i - height / 2,
+                    lowerData[i * height + (j + 1)],
+                    j + 1 - width / 2,
+                ],
+                col: colVal,
+            });
+        }
+    }
+    const positions = [];
+    const colors = [];
+    for (const vertex of vertices) {
+        positions.push(...vertex.pos);
+        colors.push(...vertex.col);
+    }
+
+    let geometry = new BufferGeometry();
+    geometry.setAttribute(
+        "position",
+        new BufferAttribute(new Float32Array(positions), 3)
+    );
+    geometry.setAttribute(
+        "color",
+        new BufferAttribute(new Float32Array(colors), 3)
+    );
+
+    geometry.computeVertexNormals();
+
+    material = new MeshPhongMaterial({
+        vertexColors: VertexColors,
+        side: DoubleSide,
+    });
+
+    let mesh = new InstancedMesh(geometry, material, 49);
+
+    //  mesh.instanceMatrix.setUsage(DynamicDrawUsage);
+
+    for (let i = 0; i < 49; i++) {
+        dummy.position.set(
+            Math.floor(i / 7) * (width - 1),
+            0,
+            Math.floor(i % 7) * (height - 1)
+        );
+        dummy.updateMatrix();
+        mesh.setMatrixAt(i, dummy.matrix);
+    }
+    mesh.instanceMatrix.needsUpdate = true;
+    mesh.position.x = -width * 3;
+    mesh.position.y = 0;
+    mesh.position.z = -height * 3;
+
+    // let mesh2 = mesh.clone();
+    // mesh2.position.y = -15;
+    // tMap.add(mesh2);
+
+    tMap.add(mesh);
+    scene.add(tMap);
+    mapID = tMap.uuid;
+    //  console.log("Terrain built");
+}
