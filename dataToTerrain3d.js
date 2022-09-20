152ad22feb2057d039b0f363a3be752c42388cb6
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-09-20 10:15:07 -0500

    Modularized animation after refactoring into a class.  Cleaned up comments.

--- dataToTerrain3d.js
+++ dataToTerrain3d.js
@@ -18,18 +18,18 @@ Using my own old template for the Three stuff (for now): https://github.com/1ofx
 Pulled my terrain generation code from 'Terranium' workup on Baconbits
 
 Todo:
-get lat long of "box" from form using data entry form
-Fix up appearance of page
 Allow user to "save" maps
 Add screenshot button to make image of canvas
-Lighting
 Palette
 styling of page in general
-modularize all of this
 
 In progress:
+Lighting
+
 
 Done:
+get lat long of "box" from form using data entry form
+modularize all of this
 speed of autorotate?
 build array of data points for request
 Figure out API request
@@ -38,6 +38,8 @@ Add Three JS structures to render animation
 make canvas bg
 */
 
+import * as animation from './testModule.js';
+
 (function () {
 
     let coord = {lat: 37.500, long: 127.600}; // Battle of ChipYongNi - lat-long to 2 decimal places
@@ -45,15 +47,9 @@ make canvas bg
     let elevationArray = [];
     let url = "";
     let bod = "";
-    let pointsDim = 50; // make a power of 10 please unless you wanna refactor
+    let pointsDim = 50;
     let totalPoints = pointsDim * pointsDim;
     let gap = 0.05 / pointsDim;
-
-    function processForm() {
-        for (let i = 0; i < 500; i += i) {
-            console.log(i);
-        }
-    }
 
     function buildRequest(posit) { // posit is an object {latitude to 2 decimal places, longitude to 2 decimal places}
         url = "https://api.open-elevation.com/api/v1/lookup";
@@ -67,15 +63,10 @@ make canvas bg
         }
         bodStr = bodStr.slice(0, bodStr.length - 1);
         bodStr += ']}';
-        //console.log(url);
-        //console.log(bodStr);
         bod = JSON.parse(bodStr);
-        //console.log(bod);
     }
 
     function getElevationFromAPI(url, input) {
-//        let elevArray = [];
-//        let resp =
         fetch(url, {
             method: 'POST',
 //               mode: 'no-cors',
@@ -88,31 +79,21 @@ make canvas bg
         })
             .then((response) => (response.json()))
             .then((data) => {
-//             console.log(data.results[0].elevation);  // elev for one point
-//             console.log("From inside: ");
                 elevationArray = [];  // clear out old data from previous request
                 for (let i = 0; i < totalPoints; i++) {
                     elevationArray.push(data.results[i].elevation)
                 }
                 ;
                 console.log(elevationArray);
-//              init();
             })
-            .then(init)
+            .then(() => {
+                let anim = new animation.Anim(elevationArray, pointsDim);
+                anim.init();
+            })
             .catch(errorMsg => {
                 console.log(errorMsg);
             });
-//          return elevArray;
     }
-
-// async function go() {
-//      document.getElementById("locDataSubmitButton").onclick = showFormData;
-//      buildRequest(coord);
-//      elevationArray = await getElevationFromAPI(url, bod);//.then(() => init());
-//     // moved init into success area of API call promise
-// }
-
-//go();
 
     document.getElementById("locDataSubmitButton").onclick = buttonClicked;
 
@@ -121,10 +102,8 @@ make canvas bg
         let latPosit = Number(document.getElementById("latitude").value);
         let longPosit = Number(document.getElementById("longitude").value);
         let newPosit = {lat: latPosit, long: longPosit};
-        document.querySelector("#titleDisplay").innerHTML = document.getElementById("title").value;
+        document.querySelector("#titleDisplay").innerHTML = titleStr;
         document.querySelector("#locationDisplay").innerHTML = newPosit.lat + " - " + newPosit.long;
-        // console.log("You clicked it. - New position:");
-        // console.log(newPosit);
         buildAMap(newPosit);
     }
 
@@ -136,250 +115,5 @@ make canvas bg
     document.querySelector("#titleDisplay").innerHTML = titleStr;
     document.querySelector("#locationDisplay").innerHTML = coord.lat + " - " + coord.long;
     buildAMap(coord);
-
-////////////////////////Three JS stuff below this line
-
-    //Three stuff:
-    let container,
-        scene,
-        camera,
-        renderer,
-        ambLt,
-        dirLt,
-        spotLt,
-        geometry,
-        material,
-        mesh,
-        controls;
-
-
-    let pallette = {
-        p1: {x: 0.61, y: 0.87, z: 0.33},
-        p2: {x: 0.72, y: 0.51, z: 0.3},
-        p3: {x: 0.38, y: 0.4, z: 0.74},
-        p4: {x: 0.38, y: 0.36, z: 0.27},
-    };
-
-
-    function init() { // I want this to go only after data is fetched
-        //console.log("From outside: ");
-        //console.log("data is: " + elevationArray);
-        setScene();
-        setCamera();
-        setLights();
-        buildRenderer();
-        buildTerrain();
-        buildTerrainFromData();
-        addOrbitControls();
-        window.addEventListener("resize", onWindowResize);
-        onWindowResize();
-        animate();
-    }
-
-    function setScene() {
-        scene = new THREE.Scene();
-        scene.background = new THREE.Color(0xaaaaaa);
-    }
-
-    function setCamera() {
-        camera = new THREE.PerspectiveCamera(
-            45,
-            window.innerWidth / window.innerHeight,
-            1,
-            1000
-        );
-        camera.position.x = 35;
-        camera.position.y = 17;
-        camera.position.z = 35;
-        scene.add(camera);
-    }
-
-    function setLights() {
-        ambLt = new THREE.AmbientLight(0xffffff, 0.5);
-        scene.add(ambLt);
-        dirLt = new THREE.DirectionalLight(0xffffff, 0.5);
-        dirLt.position.set(0, 15, 0);
-        scene.add(dirLt);
-        spotLt = new THREE.SpotLight(0xffffff, 0.5);
-        spotLt.position.set(5, 1, 2);
-        spotLt.decay = 2.0;
-        scene.add(spotLt);
-    }
-
-    function buildRenderer() {
-        // trying to put renderer into a canvas I made instead of the one Three makes:
-        // https://discourse.threejs.org/t/setting-up-renderers-canvas-element-by-html-id/13213
-        let canv = document.getElementById("display");
-        renderer = new THREE.WebGLRenderer({
-            antialias: true,
-            canvas: canv
-        });
-
-        //       renderer.setPixelRatio(window.devicePixelRatio || 1);
-//        renderer.setSize(window.innerWidth, window.innerHeight, true);
-        // or!      If ypu change this change the window resize too
-        renderer.setSize(canv.clientWidth, canv.clientHeight, false);
-        renderer.setPixelRatio((canv.clientWidth / canv.clientHeight) || 1);
-
-        //container = renderer.domElement;
-        //document.body.appendChild(container);
-        container = document.getElementById("wrapper");
-        container.appendChild(renderer.domElement);
-    }
-
-    function animate() {
-        requestAnimationFrame(animate);
-        render();
-    }
-
-    function render() {
-        updateScene();
-        renderer.render(scene, camera);
-    }
-
-    function updateScene() {
-        controls.update();
-    }
-
-    function buildTerrain() {
-        // right now we are just throwing a standard block animation into the canvas.
-        // Later we will build terrain with elevationArray.  -  se funtion below
-        //       geometry = new THREE.BoxGeometry(1, 1, 1);
-        geometry = new THREE.SphereGeometry(1);
-        material = new THREE.MeshPhongMaterial({
-            color: "red",
-            side: THREE.DoubleSide,
-        });
-        mesh = new THREE.Mesh(geometry, material);
-        scene.add(mesh);
-        //console.log(scene);
-    }
-
-    function buildTerrainFromData() {
-        let maxElev = Math.max(...elevationArray);
-        let minElev = Math.min(...elevationArray);
-        let elevDiff = maxElev - minElev;
-        //resize elev data
-        for (let i = 0; i < elevationArray.length; i++) {
-            elevationArray[i] = (elevationArray[i] / 100.0) - (maxElev / 100.0);
-        }
-        //console.log('After shift: ' + elevationArray);
-        // build
-        let vertices = [];
-        for (let i = 0; i < pointsDim - 1; i++) {
-            for (let j = 0; j < pointsDim - 1; j++) {
-                let idx = (i * pointsDim) + j;
-                let cv = getPalletteSample(elevationArray[idx] / maxElev, pallette);
-                let colVal = [cv.r, cv.g, cv.b];
-                vertices.push({
-                    pos: [i - pointsDim / 2, elevationArray[i * pointsDim + j], j - pointsDim / 2],
-                    col: colVal,
-                });
-                vertices.push({
-                    pos: [
-                        i + 1 - pointsDim / 2,
-                        elevationArray[(i + 1) * pointsDim + j],
-                        j - pointsDim / 2,
-                    ],
-                    col: colVal,
-                });
-                vertices.push({
-                    pos: [
-                        i - pointsDim / 2,
-                        elevationArray[i * pointsDim + (j + 1)],
-                        j + 1 - pointsDim / 2,
-                    ],
-                    col: colVal,
-                });
-                vertices.push({
-                    pos: [
-                        i + 1 - pointsDim / 2,
-                        elevationArray[(i + 1) * pointsDim + j],
-                        j - pointsDim / 2,
-                    ],
-                    col: colVal,
-                });
-                vertices.push({
-                    pos: [
-                        i + 1 - pointsDim / 2,
-                        elevationArray[(i + 1) * pointsDim + (j + 1)],
-                        j + 1 - pointsDim / 2,
-                    ],
-                    col: colVal,
-                });
-                vertices.push({
-                    pos: [
-                        i - pointsDim / 2,
-                        elevationArray[i * pointsDim + (j + 1)],
-                        j + 1 - pointsDim / 2,
-                    ],
-                    col: colVal,
-                });
-            }
-        }
-        const positions = [];
-        const colors = [];
-        for (const vertex of vertices) {
-            positions.push(...vertex.pos);
-            colors.push(...vertex.col);
-        }
-        let geometry = new THREE.BufferGeometry();
-        geometry.setAttribute(
-            "position",
-            new THREE.BufferAttribute(new Float32Array(positions), 3)
-        );
-        geometry.setAttribute(
-            "color",
-            new THREE.BufferAttribute(new Float32Array(colors), 3)
-        );
-
-        geometry.computeVertexNormals();
-
-        material = new THREE.MeshPhongMaterial({
-            vertexColors: THREE.VertexColors,
-            side: THREE.DoubleSide,
-        });
-
-        let mesh = new THREE.Mesh(geometry, material);
-        scene.add(mesh);
-    }
-
-    function addOrbitControls() {
-        controls = new THREE.OrbitControls(camera, container);
-        controls.minDistance = 5;
-        controls.maxDistance = 1500;
-        controls.autoRotate = true;
-        controls.autoRotateSpeed = 1;
-    }
-
-    function getPalletteSample(t, pallette) {
-        // adapted from Inigio Quilez: https://iquilezles.org/articles/palettes/
-        let re =
-            pallette.p1.x +
-            pallette.p2.x * Math.cos(2 * Math.PI * (pallette.p3.x * t + pallette.p4.x));
-        let gr =
-            pallette.p1.y +
-            pallette.p2.y * Math.cos(2 * Math.PI * (pallette.p3.y * t + pallette.p4.y));
-        let bl =
-            pallette.p1.z +
-            pallette.p2.z * Math.cos(2 * Math.PI * (pallette.p3.z * t + pallette.p4.z));
-        let c = new THREE.Color(re, gr, bl);
-        return c;
-    }
-
-    function onWindowResize() {
-        // camera.aspect = window.innerWidth / window.innerHeight;
-        // camera.updateProjectionMatrix();
-        // renderer.setSize(window.innerWidth, window.innerHeight);
-// or
-        let canv = document.getElementById("display");
-
-        camera.aspect = canv.clientWidth / canv.clientHeight;
-        renderer.setPixelRatio((canv.clientWidth / canv.clientHeight) || 1);
-
-        camera.updateProjectionMatrix();
-        //       renderer.setSize(canv.clientWidth, canv.clientHeight);
-    }
-
 
 }());
---
+++ testModule.js
@@ -0,0 +1,251 @@
+class Anim {
+    //Three stuff:
+    elevationArray;
+    container;
+    scene;
+    camera;
+    renderer;
+    ambLt;
+    dirLt;
+    spotLt;
+    geometry;
+    material;
+    mesh;
+    controls;
+    palette;
+    pointsDim;
+
+    constructor(_elevArray, _pointDimension) {
+        this.elevationArray = _elevArray;
+        this.palette = {
+            p1: {x: 0.61, y: 0.87, z: 0.33},
+            p2: {x: 0.72, y: 0.51, z: 0.3},
+            p3: {x: 0.38, y: 0.4, z: 0.74},
+            p4: {x: 0.38, y: 0.36, z: 0.27},
+        }
+        this.pointsDim = _pointDimension;
+    }
+
+    init() { // I want this to go only after data is fetched
+        //console.log("From outside: ");
+        //console.log("data is: " + elevationArray);
+        this.setScene();
+        this.setCamera();
+        this.setLights();
+        this.buildRenderer();
+        this.buildTerrain();
+        this.buildTerrainFromData();
+        this.addOrbitControls();
+        window.addEventListener("resize", this.onWindowResize);
+        this.onWindowResize();
+        this.animate();
+    }
+
+    setScene() {
+        this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color(0xaaaaaa);
+    }
+
+    setCamera() {
+        this.camera = new THREE.PerspectiveCamera(
+            45,
+            window.innerWidth / window.innerHeight,
+            1,
+            1000
+        );
+        this.camera.position.x = 35;
+        this.camera.position.y = 17;
+        this.camera.position.z = 35;
+        this.scene.add(this.camera);
+    }
+
+    setLights() {
+        this.ambLt = new THREE.AmbientLight(0xffffff, 0.5);
+        this.scene.add(this.ambLt);
+        this.dirLt = new THREE.DirectionalLight(0xffffff, 0.5);
+        this.dirLt.position.set(0, 15, 0);
+        this.scene.add(this.dirLt);
+        this.spotLt = new THREE.SpotLight(0xffffff, 0.5);
+        this.spotLt.position.set(5, 1, 2);
+        this.spotLt.decay = 2.0;
+        this.scene.add(this.spotLt);
+    }
+
+    buildRenderer() {
+        // trying to put renderer into a canvas I made instead of the one Three makes:
+        // https://discourse.threejs.org/t/setting-up-renderers-canvas-element-by-html-id/13213
+        let canv = document.getElementById("display");
+        this.renderer = new THREE.WebGLRenderer({
+            antialias: true,
+            canvas: canv
+        });
+
+        //       renderer.setPixelRatio(window.devicePixelRatio || 1);
+//        renderer.setSize(window.innerWidth, window.innerHeight, true);
+        // or!      If ypu change this change the window resize too
+        this.renderer.setSize(canv.clientWidth, canv.clientHeight, false);
+        this.renderer.setPixelRatio((canv.clientWidth / canv.clientHeight) || 1);
+
+        //container = renderer.domElement;
+        //document.body.appendChild(container);
+        this.container = document.getElementById("wrapper");
+        this.container.appendChild(this.renderer.domElement);
+    }
+
+    animate() {
+        requestAnimationFrame(() => this.animate());
+        this.render();
+    }
+
+    render() {
+        this.updateScene();
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    updateScene() {
+        this.controls.update();
+    }
+
+    buildTerrain() {
+        // right now we are just throwing a standard block animation into the canvas.
+        // Later we will build terrain with elevationArray.  -  se function below
+        //       geometry = new THREE.BoxGeometry(1, 1, 1);
+        let geometry = new THREE.SphereGeometry(1);
+        let material = new THREE.MeshPhongMaterial({
+            color: "red",
+            side: THREE.DoubleSide,
+        });
+        let mesh = new THREE.Mesh(geometry, material);
+        this.scene.add(mesh);
+        //console.log(scene);
+    }
+
+    buildTerrainFromData() {
+        let maxElev = Math.max(...this.elevationArray);
+        let minElev = Math.min(...this.elevationArray);
+        let elevDiff = maxElev - minElev;
+        //resize elev data
+        for (let i = 0; i < this.elevationArray.length; i++) {
+            this.elevationArray[i] = (this.elevationArray[i] / 100.0) - (maxElev / 100.0);
+        }
+        //console.log('After shift: ' + elevationArray);
+        // build
+        let vertices = [];
+        for (let i = 0; i < this.pointsDim - 1; i++) {
+            for (let j = 0; j < this.pointsDim - 1; j++) {
+                let idx = (i * this.pointsDim) + j;
+                let cv = this.getPalletteSample(this.elevationArray[idx] / maxElev, this.palette);
+                let colVal = [cv.r, cv.g, cv.b];
+                vertices.push({
+                    pos: [i - this.pointsDim / 2, this.elevationArray[i * this.pointsDim + j], j - this.pointsDim / 2],
+                    col: colVal,
+                });
+                vertices.push({
+                    pos: [
+                        i + 1 - this.pointsDim / 2,
+                        this.elevationArray[(i + 1) * this.pointsDim + j],
+                        j - this.pointsDim / 2,
+                    ],
+                    col: colVal,
+                });
+                vertices.push({
+                    pos: [
+                        i - this.pointsDim / 2,
+                        this.elevationArray[i * this.pointsDim + (j + 1)],
+                        j + 1 - this.pointsDim / 2,
+                    ],
+                    col: colVal,
+                });
+                vertices.push({
+                    pos: [
+                        i + 1 - this.pointsDim / 2,
+                        this.elevationArray[(i + 1) * this.pointsDim + j],
+                        j - this.pointsDim / 2,
+                    ],
+                    col: colVal,
+                });
+                vertices.push({
+                    pos: [
+                        i + 1 - this.pointsDim / 2,
+                        this.elevationArray[(i + 1) * this.pointsDim + (j + 1)],
+                        j + 1 - this.pointsDim / 2,
+                    ],
+                    col: colVal,
+                });
+                vertices.push({
+                    pos: [
+                        i - this.pointsDim / 2,
+                        this.elevationArray[i * this.pointsDim + (j + 1)],
+                        j + 1 - this.pointsDim / 2,
+                    ],
+                    col: colVal,
+                });
+            }
+        }
+        const positions = [];
+        const colors = [];
+        for (const vertex of vertices) {
+            positions.push(...vertex.pos);
+            colors.push(...vertex.col);
+        }
+        let geometry = new THREE.BufferGeometry();
+        geometry.setAttribute(
+            "position",
+            new THREE.BufferAttribute(new Float32Array(positions), 3)
+        );
+        geometry.setAttribute(
+            "color",
+            new THREE.BufferAttribute(new Float32Array(colors), 3)
+        );
+
+        geometry.computeVertexNormals();
+
+        let material = new THREE.MeshPhongMaterial({
+            vertexColors: THREE.VertexColors,
+            side: THREE.DoubleSide,
+        });
+
+        let mesh = new THREE.Mesh(geometry, material);
+        this.scene.add(mesh);
+    }
+
+    addOrbitControls() {
+        this.controls = new THREE.OrbitControls(this.camera, this.container);
+        this.controls.minDistance = 5;
+        this.controls.maxDistance = 1500;
+        this.controls.autoRotate = true;
+        this.controls.autoRotateSpeed = 1;
+    }
+
+    getPalletteSample(t, palette) {
+        // adapted from Inigio Quilez: https://iquilezles.org/articles/palettes/
+        let re =
+            palette.p1.x +
+            palette.p2.x * Math.cos(2 * Math.PI * (palette.p3.x * t + palette.p4.x));
+        let gr =
+            palette.p1.y +
+            palette.p2.y * Math.cos(2 * Math.PI * (palette.p3.y * t + palette.p4.y));
+        let bl =
+            palette.p1.z +
+            palette.p2.z * Math.cos(2 * Math.PI * (palette.p3.z * t + palette.p4.z));
+        let c = new THREE.Color(re, gr, bl);
+        return c;
+    }
+
+    onWindowResize() {
+        // camera.aspect = window.innerWidth / window.innerHeight;
+        // camera.updateProjectionMatrix();
+        // renderer.setSize(window.innerWidth, window.innerHeight);
+// or
+        let canv = document.getElementById("display");
+
+        this.camera.aspect = canv.clientWidth / canv.clientHeight;
+        this.renderer.setPixelRatio((canv.clientWidth / canv.clientHeight) || 1);
+
+        this.camera.updateProjectionMatrix();
+        //       renderer.setSize(canv.clientWidth, canv.clientHeight);
+    }
+
+}
+
+export {Anim};
